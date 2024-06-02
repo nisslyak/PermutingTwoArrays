0269783c9593bef8c1be3a3f7fe4b44943474094
nisslyak <nissopodolyak@gmail.com>
2024-06-02 06:00:19 +0300

    First commit

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+   public static string twoArrays(int k, List<int> A, List<int> B)
+    {
+        A.Sort();
+        B.Sort();
+        B.Reverse();
+
+        bool keepLooping = true;
+
+        for (int i = 0; i < A.Count; i++)
+        {
+            while (keepLooping)
+            {
+                if (A[i] + B[i] < k)
+                {
+                    keepLooping = false;
+                    return "NO";
+                }
+                break;
+            }
+        }
+        return "YES";
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int k = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> A = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(ATemp => Convert.ToInt32(ATemp)).ToList();
+
+            List<int> B = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(BTemp => Convert.ToInt32(BTemp)).ToList();
+
+            string result = Result.twoArrays(k, A, B);
+
+            Console.WriteLine(result);
+        }
+    }
+}
